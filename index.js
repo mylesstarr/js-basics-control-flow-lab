433f21000127672f142e8f2cbe7eb4d56d7111f4
Myles Starr <mylesstarr@gmail.com>
2021-04-26 17:54:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) 
+  { return "This one is on me!"
+} else if (feet >= 2000 && feet <= 2500) {
+  return "I will gladly take your thirty bucks." 
+} else if (feet >= 2500) {
+  return "No can do."
+} 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+// function switchOnCharmFromTip(tip){
+//   let generous = (tip >= 10) 
+//   let notSoGenerous = (tip >= 5 && tip <= 10)
+//   let 
+  
+  // if (tip = generous) {
+  //   return "Thank you so much."
+  // } else if (tip >= 5 && tip <= 10) {
+  //   return "Thank you."
+  // } else if (tip <= 5) {
+  //   return "Bye."}
+// }
+
+
+
+// let switchOnCharmForTip = (tip) => {
+
+// let generous = (tip >= 10)
+// let notAsGenerous = (tip >= 5 && tip <= 10)
+// let anythingElse = (tip <= 5)
+
+  // switch("tip"){
+  //     case tip = "generous":
+  //         return "Thank you so much."
+  //         break;
+  //     case tip = "notAsGenerous":
+  //         return "Thank you."
+  //         break;
+  //     default:
+  //         return "Bye."
+  //         break;
+  //       }
+  //     }
+
+  function switchOnCharmFromTip(tip){
+
+  if (tip == "generous") {
+    return "Thank you so much."
+  } else if (tip == "not as generous") {
+    return "Thank you."
+  } else {
+    return "Bye."}
+  }
+
